83e784dafd0f32576938652f5043c31472ae44e3
Dorgenark <simonromainville@hotmail.com>
2018-04-16 16:59:01 +0200

    menu + page intro fonctionnels!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// MENU ANIMATION
 toggle = document.querySelectorAll(".toggle")[0];
 nav = document.querySelectorAll("nav")[0];
 toggle_open_text = '&#8601;';
@@ -5,11 +6,13 @@ toggle_close_text = '&#8599;';
 
 toggle.addEventListener('click', function() {
 	nav.classList.toggle('open');
+	$(".submenu").css("position", "inherit");
 	
   if (nav.classList.contains('open')) {
     toggle.innerHTML = toggle_close_text;
   } else {
     toggle.innerHTML = toggle_open_text;
+    $(".submenu").css("position", "relative");
   }
 }, false);
 
@@ -18,7 +21,28 @@ setTimeout(function(){
 }, 150);
 
 
-
+// MENU LINKS
 $(".l5").click(function(){
 	window.location.href = "index.html";
-})
+});
+
+
+
+// PRESENTATION TEXT DISPLAY
+$(".submenu").on("click", "#textcomics-btn", function(){
+	$("#textcomics").show(400);
+	$("#textfilms").hide(300);
+	$("#textgames").hide(300);
+});
+
+$(".submenu").on("click", "#textfilms-btn", function(){
+	$("#textcomics").hide(300);
+	$("#textfilms").show(400);
+	$("#textgames").hide(300);
+});
+
+$(".submenu").on("click", "#textgames-btn", function(){
+	$("#textcomics").hide(300);
+	$("#textfilms").hide(300);
+	$("#textgames").show(400);
+});
